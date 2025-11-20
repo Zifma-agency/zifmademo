e2ebf023adb92ef28552c6e63366428d5aa36846
Zifma-agency <zifma.agency@gmail.com>
2025-11-20 15:57:57 +0530

    Update hero-with-logos.tsx

--- src/components/ui/hero-with-logos.tsx
+++ src/components/ui/hero-with-logos.tsx
@@ -87,7 +87,7 @@ const HeroWithLogos = () => {
       
       {/* Logo */}
       <img 
-        src={Zilaxis} 
+        src={zilaxisLogo} 
         alt="Zilaxis Media" 
         className="absolute -top-6 left-6 z-20 h-32 md:h-44 w-auto"
       />
