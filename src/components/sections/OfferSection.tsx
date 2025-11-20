9c9993ce97ad477443ece75373007a3ae42e6658
Zifma-agency <zifma.agency@gmail.com>
2025-11-20 14:10:31 +0530

    Update OfferSection.tsx

--- src/components/sections/OfferSection.tsx
+++ src/components/sections/OfferSection.tsx
@@ -126,7 +126,8 @@ const OfferSection = () => {
           </p>
         </motion.div>
         
-        <div className="flex flex-wrap justify-center gap-6 md:gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 justify-center">
+
           {/* Starter Clinic Website */}
           <motion.div
             initial={{ opacity: 0, y: 20 }}
